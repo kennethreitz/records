018d0f086168a36261c1987537c9a822838051a2
Kenneth Reitz <me@kennethreitz.org>
2016-02-06 21:42:49 -0500

    coming together

--- proto.py
+++ proto.py
@@ -6,9 +6,11 @@ from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-
-from psycopg2.extras import RealDictConnection, register_hstore
+import tablib
+import psycopg2
+from psycopg2.extras import RealDictConnection, register_hstore, RealDictCursor, NamedTupleCursor, DictCursor
 from psycopg2 import ProgrammingError
+from datetime import datetime
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
@@ -65,12 +67,49 @@ class SessionPropertyMixin(object):
         if close:
             session.close()
 
+def reduce_datetimes(row):
+    for i in range(len(row)):
+        if isinstance(row[i], datetime):
+            row[i] = '{}'.format(row[i])
+    return row
+
+class ResultSet(object):
+    def __init__(self, rows):
+        self._rows = rows
+        self._all_rows = []
+
+    def _fetch_all(self):
+        return list(self._rows)
+
+    @property
+    def dataset(self):
+        data = tablib.Dataset()
+        data.headers = self.all[0].keys()
+
+        for row in self.all:
+            row = [v for k, v in row.items()]
+            row = reduce_datetimes(row)
+            data.append(row)
+
+        return data
+
+    @property
+    def all(self):
+        if not self._all_rows:
+            self._all_rows = self._fetch_all()
+        return self._all_rows
+
+    def __getattr__(self, attr):
+        fallback_attr = getattr(self._rows, attr)
+        return getattr(object, attr, fallback_attr)
 
 
-class Warehouse(object):
+
+
+class Database(object):
     def __init__(self, conn_str):
         self._conn_str = conn_str
-        self.db = RealDictConnection(conn_str)
+        self.db = psycopg2.connect(conn_str, cursor_factory=RealDictCursor)
 
         # Enable hstore if it's available.
         self._enable_hstore()
@@ -90,9 +129,10 @@ class Warehouse(object):
 
         # Row-by-row result generator.
         gen = (r for r in c)
+        return ResultSet(gen)
 
         # If fetchall is True, return a list.
-        return list(gen) if fetchall else gen
+        # return list(gen) if fetchall else gen
 
     def query_file(self, path, params=None, fetchall=False):
         # Read the given .sql file into memory.

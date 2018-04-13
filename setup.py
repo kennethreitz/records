76afc6fd67247267d3d4d9bcf84558d3c6d6d53a
Jan Vlcinsky <jan.vlcinsky@tamtamresearch.com>
2018-04-14 00:09:07 +0200

    Removed support for python 2.6. Fix #141

--- setup.py
+++ setup.py
@@ -84,12 +84,12 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
     ),
     cmdclass={

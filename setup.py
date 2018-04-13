61b2b68bd5bbbb58f9d47e0e6799f07f61db2f1c
Jan Vlcinsky <jan.vlcinsky@tamtamresearch.com>
2018-04-13 23:33:39 +0200

    Remove utf-8 chars "..." from `setup.py`

--- setup.py
+++ setup.py
@@ -32,15 +32,15 @@ class PublishCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds…')
+            self.status('Removing previous builds...')
             rmtree(os.path.join(here, 'dist'))
         except FileNotFoundError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
+        self.status('Building Source and Wheel (universal) distribution...')
         os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
 
-        self.status('Uploading the package to PyPi via Twine…')
+        self.status('Uploading the package to PyPi via Twine...')
         os.system('twine upload dist/*')
 
         sys.exit()

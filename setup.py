59bb3c6da902a3fb9d71524daf740655a45529a5
Hugo <hugovk@users.noreply.github.com>
2018-04-26 11:45:47 +0300

    Use automatic formatters

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ class PublishCommand(Command):
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print('\033[1m{}\033[0m'.format(s))
 
     def initialize_options(self):
         pass
@@ -38,7 +38,7 @@ class PublishCommand(Command):
             pass
 
         self.status('Building Source and Wheel (universal) distribution...')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        os.system('{} setup.py sdist bdist_wheel --universal'.format(sys.executable))
 
         self.status('Uploading the package to PyPi via Twine...')
         os.system('twine upload dist/*')

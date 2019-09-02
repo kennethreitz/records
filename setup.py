28aac298e0ac696778292f8239bb27929cdb0fa0
Paul Philip Voelker <p.voelker@campus.tu-berlin.de>
2019-09-02 18:51:51 -0400

    remove temporary openpyxl fix

--- setup.py
+++ setup.py
@@ -47,7 +47,6 @@ class PublishCommand(Command):
 
 requires = ['SQLAlchemy;python_version>="3.0"',
             'SQLAlchemy<1.1;python_version<"3.0"',
-            'openpyxl<2.5.0', # temporary fix to issue #142
             'tablib>=0.11.4',
             'docopt']
 version = '0.5.3'
